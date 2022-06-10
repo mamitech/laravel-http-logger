74142d5530723363966954073cda41b147b97554
arfn <arifin@mamiteam.com>
2022-06-10 12:46:34 +0700

    publish config

--- src/HttpLoggerServiceProvider.php
+++ src/HttpLoggerServiceProvider.php
@@ -6,9 +6,18 @@ use Illuminate\Support\ServiceProvider;
 
 class HttpLoggerServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/config.php' => config_path('laravel-http-logger.php'),
+            ], 'config');
+        }
+    }
+
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'laravel-http-logger');
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'laravel-http-logger');
 
         $this->app->singleton('laravel-http-logger', function () {
             return new HttpLogger;
